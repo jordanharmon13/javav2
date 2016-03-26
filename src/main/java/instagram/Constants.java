e6478b47ecf628a25a1f237f6866dc6cfffde296
Jordan Harmon <Jordan Harmon>
2016-03-26 14:40:05 -0600

    changes

---
+++ src/main/java/instagram/Constants.java
@@ -0,0 +1,20 @@
+package instagram;
+
+public final class Constants {
+
+    /**
+     * Config Properties
+     */
+    public static final String CLIENT_ID = "cb8f2beb45ae42958d3105c17edc95b9";
+    public static final String CLIENT_SECRET = "c2f8ad98d4d04d948fec8e9585d6c0e6";
+    public static final String REDIRECT_URI = "http://java-dlee.rhcloud.com/insta/handleinstaOperations/";
+
+
+    /**
+     * Http Session Attributes
+     */
+    public static final String INSTAGRAM_OBJECT = "igObject";
+    public static final String INSTAGRAM_SERVICE = "igService";
+
+    public static final int MAX_PAGE_SIZE = 5;
+}
