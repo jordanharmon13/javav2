84f4d971a56f7bbfcf58745a260318cfbc679bc8
Jordan Harmon <Jordan Harmon>
2016-04-01 18:26:06 -0600

    changes

--- src/main/java/instagram/LoadPhotos.java
+++ src/main/java/instagram/LoadPhotos.java
@@ -46,15 +46,15 @@ public class LoadPhotos extends HttpServlet {
             double lat, lng;
 
 
-            for(int i = 0; i < map.size(); i++) {              
+            /*for(int i = 0; i < map.size(); i++) {              
                lat = Double.parseDouble(map.get(i).get("latitude"));
                lng = Double.parseDouble(map.get(i).get("longitude"));
                
                if( geo.checkLng(lng) && geo.checklat(lat) )
                    searchMap.add(map.get(i));  
-            }
-            request.setAttribute("map", searchMap);
-            request.getRequestDispatcher("photos.jsp").forward(request, response);
+            }*/
+            //request.setAttribute("map", searchMap);
+            //request.getRequestDispatcher("photos.jsp").forward(request, response);
             
             out.println("<!DOCTYPE html>");
             out.println("<html>");
@@ -63,9 +63,10 @@ public class LoadPhotos extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet LoadPhotos at " + request.getContextPath() + "</h1>");
-            for (int i = 0; i < searchMap.size(); i++) {
+            /*for (int i = 0; i < searchMap.size(); i++) {
                 out.println(searchMap.get(i));
-            }
+            }*/
+            out.println(geoString);
             out.println("</body>");
             out.println("</html>");
         }
